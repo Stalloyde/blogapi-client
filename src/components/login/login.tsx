51a7f91d692f7fd592e22c263dac4bb2daf9a16e
Stalloyde <stalloyde93@gmail.com>
2024-01-20 22:33:41 +0800

    Style login page

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,22 +1,47 @@
 import { useEffect, useState } from 'react';
 import Layout from '../layout/layout';
+import styles from './login.module.css';
+import usernameIcon from '../../assets/icons8-username-64.png';
+import passwordIcon from '../../assets/icons8-password-50.png';
 
 function Login() {
-  const [data, setData] = useState();
+  // const [data, setData] = useState();
 
-  useEffect(() => {
-    const a = async () => {
-      const response = await fetch('http://localhost:3000/login', { mode: 'cors' });
-      const responseData = await response.json();
-      setData(responseData);
-    };
-    a();
-  }, []);
+  // useEffect(() => {
+  //   const a = async () => {
+  //     const response = await fetch('http://localhost:3000/login', { mode: 'cors' });
+  //     const responseData = await response.json();
+  //     setData(responseData);
+  //   };
+  //   a();
+  // }, []);
 
   return (
-  <Layout>
-    {data}
-  </Layout>
+    <Layout>
+      <>
+        <div className={styles.mainHeader}>
+          <h2>LOGIN</h2>
+        </div>
+
+        <div className={styles.content}>
+          <form method='post' className={styles.form}>
+            <div>
+              <img src={usernameIcon} alt='username' />
+              <input type='text' id='username' placeholder='Username' />
+            </div>
+
+            <div>
+              <img src={passwordIcon} alt='password' />
+              <input type='password' id='password' placeholder='Password' />
+            </div>
+
+            <div>
+              <button>Log In</button>
+            </div>
+          </form>
+        </div>
+      </>
+    </Layout>
   );
 }
 
